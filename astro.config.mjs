f2bb15fafd2ef77faf7366e2a2e161ba1b9b47b4
Tu Nombre <tu@correo.com>
2026-09-19 17:19:09 +0200

    Add XML sitemap with Astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://correodos.github.io/mia-proyecto/',
   base: '/mia-proyecto/',
+  integrations: [sitemap()],
 });
