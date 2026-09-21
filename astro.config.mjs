9efc19d8a3f77d20e41be729cf03edbc7ef12eb4
Tu Nombre <tu@correo.com>
2026-09-22 00:23:08 +0200

    Corrige rutas para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   output: 'static',
   site: 'https://correodos.github.io/mia-proyecto/',
+  base: '/mia-proyecto/',
   integrations: [sitemap()],
 });
